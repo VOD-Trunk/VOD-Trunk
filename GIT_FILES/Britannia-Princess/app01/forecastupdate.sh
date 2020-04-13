ff73c72c7a6cd3d0fe99931b91dd64525d068bcc
VOD-Trunk <deepam.1920@hsc.com>
2020-04-13 12:10:26 -0400

    Updated files commited on Mon 13 Apr 2020 12:10:25 PM EDT

--- GIT_FILES/Britannia-Princess/app01/forecastupdate.sh
+++ GIT_FILES/Britannia-Princess/app01/forecastupdate.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 
-
-
-
-
+#echo >>/root/weatherupdate/refreshcurrent.log
+#date >>/root/weatherupdate/refreshcurrent.log
+#/usr/bin/curl -s "http://admin.britanniavod.carnivaluk.com/location/private/refreshcurrent" >>/root/weatherupdate/refreshcurrent.log 2>&1
 
 echo >>/root/weatherupdate/refreshforecast.log
 date >>/root/weatherupdate/refreshforecast.log
--- GIT_FILES/ZUDM/Britannia-Princess/app02/currentupdate.sh
+++
@@ -1,9 +0,0 @@
-
-
-echo >>/root/weatherupdate/refreshcurrent.log
-date >>/root/weatherupdate/refreshcurrent.log
-/usr/bin/curl -s "http://admin.britanniavod.carnivaluk.com/location/private/refreshcurrent" >>/root/weatherupdate/refreshcurrent.log 2>&1
-
-
-
-
