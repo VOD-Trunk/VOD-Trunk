662bc72cd7d997cd50d9aad98ba5992fe2bb48de
VOD-Trunk <deepam.1920@hsc.com>
2020-04-14 08:43:56 -0400

    Updatng scripts

--- script/Config_comparison_automation/fetch_files.sh
+++ script/Config_comparison_automation/fetch_files.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
-config_file_path_local='/home/abhishek/Config_Automation/Config_Files'
 git_path='/home/abhishek/deepam/VOD-Trunk'
 temp='/home/abhishek/Config_Automation/temp.txt'
 diff_path='/home/abhishek/deepam/VOD-Trunk/ship_git_diff.csv'
+ip='/home/abhishek/Config_Automation/ip.txt'
 USERNAME=root
 PASS="not4dev!"
-echo "SHIP NAME, FILE NAME, SERVER NAME, SHIP VALUES, GIT VALUES, DECISION" >$diff_path
-for line in `cat ip.txt`
+echo "SHIP NAME,FILE NAME,SERVER NAME,SHIP VALUES,GIT VALUES,DECISION" >$diff_path
+echo "Pulling files from GIT"
+echo
+cd ${git_path}
+git checkout develop
+git pull origin develop
+echo "======================================================================================================================="
+echo
+for line in `cat $ip`
 do
     HOSTNAME=`echo $line | cut -d ',' -f 2`
 	ship=`echo $line | cut -d ',' -f 1`
-	echo "Fetching files from $ship"
-	echo $HOSTNAME
-	sshpass -p ${PASS} ssh ${USERNAME}@${HOSTNAME} '/home/config_files/fetch_files.sh'
-	servers="app01 app02 media01 media02 lb01 lb02"
+	echo "Copying files from all six servers to /home/config_files in app01 server of $ship"
+	echo "======================================================================================================================="
+	echo
+	
+	#Run the script fetch_files.sh on respective servers and move to next hostname if ssh takes more than 20 seconds.
+	sshpass -p ${PASS} ssh -o ConnectTimeout=20 ${USERNAME}@${HOSTNAME} '/home/config_files/fetch_files.sh' 2>/dev/null
+	e=$?
+	if [ "$e" != "0" ]
+	then
+		continue
+	fi
+	
+	echo "Copying files app01 of $ship to local GIT path."
+	echo "======================================================================================================================="
+	echo
+	#Copy the consolidated config files from server to local git path.
+	sshpass -p ${PASS} scp -r ${USERNAME}@${HOSTNAME}:/home/config_files/* ${git_path}/SHIP_FILES/${ship}
+	echo "Comparing files just fetched from ship with corresponding file in GIT."
+	echo "======================================================================================================================="
+	echo
+	#compare SHIP files just copied with GIT files pulled from GIT.
+	servers="app01 media01 lb01"
 	for server in $servers
 	do
-		sudo rm -f ${config_file_path_local}/${ship}/${server}/*
-		sshpass -p ${PASS} scp -r ${USERNAME}@${HOSTNAME}:/home/config_files/$server ${config_file_path_local}/${ship}
-		echo "Server name : $server"
-		cd /home/abhishek/deepam/VOD-Trunk
-		git checkout develop
-		git pull origin develop
-
-		echo ${PASS} | sudo rm -f ${git_path}/SHIP_FILES/${ship}/${server}/*
-		cp -r ${config_file_path_local}/${ship}/${server} ${git_path}/SHIP_FILES/${ship}
-
 		for file in `ls ${git_path}/SHIP_FILES/${ship}/${server}`
 		do
 			a="${git_path}/SHIP_FILES/${ship}/${server}/${file}"
@@ -35,17 +50,30 @@ do
 			then
 				ship_file_values=`grep "<" $temp | cut -c 3- | sed ':a;N;$!ba;s/\n/;/g'`
 				master_file_values=`grep ">" $temp | cut -c 3- | sed ':a;N;$!ba;s/\n/;/g'`
-				echo "${ship}, ${file}, ${server}, $ship_file_values, $master_file_values" >>$diff_path
+				echo "${ship},${file},${server},$ship_file_values,$master_file_values">>$diff_path
 			fi
 		done
-    	done
+	done
+	echo "Please find the differences in $diff_path"
+	echo "======================================================================================================================="
+	echo
 done
 
+echo "Sending the diff URL to all stake holders. --Email config not present currently, so skipping this part."
+echo "======================================================================================================================="
+echo
+if [ 1 == 2 ]
+then
 line_count=`wc -l $diff_path`
 if [ $line_count -gt 1 ]
 then
 	echo "https://github.com/VOD-Trunk/VOD-Trunk/blob/develop/ship_git_diff.csv" | sendmail abhishek.chadha@hsc.com
 fi
+fi
+
+echo "Pushing the ship files and diff file in GIT."
+echo "======================================================================================================================="
+echo
 
 git add .
 git commit -m "Updated files commited on `date`"
--- script/Config_comparison_automation/git_push.sh
+++ script/Config_comparison_automation/git_push.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 git_path='/home/abhishek/deepam/VOD-Trunk'
 diff_path='/home/abhishek/deepam/VOD-Trunk/ship_git_diff.csv'
+line_count=`wc -l $diff_path`
+if [ $line_count -gt 1 ]
+then
+	echo "Reading the csv file for decisions"
+	echo "=========================================================================="
+	LINES=`cat $diff_path`
+	IFS=$'\n'
+	for line in $LINES
+	do
+		ship=`echo $line | cut -d ',' -f 1`
+		file=`echo $line | cut -d ',' -f 2`
+		server=`echo $line | cut -d ',' -f 3`
+		decision=`echo $line | cut -d ',' -f 6`
+		if [ "$decision" == "YES" ] || [ "$decision" == "yes" ] || [ "$decision" == "Yes" ] || [ "$decision" == " YES" ] || [ "$decision" == " yes" ]
+		then
+			echo "Diff found at $ship $server $file $decision"
+			cd $git_path
+			git checkout develop
+			git pull origin develop
+			cp $git_path/SHIP_FILES/$ship/$server/$file $git_path/GIT_FILES/$ship/$server/$file
+			git add .
+			git commit -m "Updating $ship GIT files with updated values on $ship"
+			
+			git checkout master
+			git pull origin master
+			git add .
+			git commit -m "Updating $ship master GIT files with updated values on $ship"
+			git checkout develop $git_path/GIT_FILES/$ship/$server/$file
+		fi
+	done
 
-
-cd $git_path
-git checkout develop
-git pull origin develop
-
-LINES=`cat $diff_path`
-IFS=$'\n'
-for line in $LINES
-do
-	ship=`echo $line | cut -d ',' -f 1`
-	file=`echo $line | cut -d ',' -f 2`
-	server=`echo $line | cut -d ',' -f 3`
-	decision=`echo $line | cut -d ',' -f 6`
-	if [ "$decision" == "YES" ] || [ "$decision" == "yes" ] || [ "$decision" == "Yes" ] || [ "$decision" == " YES" ]
-	then
-		cp $git_path/SHIP_FILES/$ship/$server/$file $git_path/GIT_FILES/$ship/$server/$file
-		git add .
-		git commit -m "Updating $ship GIT files with updated values on $ship"
-		git push origin develop
-		
-		git checkout master
-		git pull origin master
-		git checkout develop $git_path/GIT_FILES/$ship/$server/$file
-		git add .
-		git commit -m "Updating $ship master GIT files with updated values on $ship"
-		git push origin master
-	fi
-done
+	git push origin develop
+	git push origin master
+fi
