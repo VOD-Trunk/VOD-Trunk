96b635953a937f91f00abb7d14e1538bdd54cc8e
vod <abhishek.chadha@hsc.com>
2020-07-01 04:31:43 -0400

    Updated on Wed 01 Jul 2020 04:31:43 AM EDT

--- Ship_Current_Files/Britannia-Princess/app02/currentupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo >>/root/weatherupdate/refreshcurrent.log
-date >>/root/weatherupdate/refreshcurrent.log
-/usr/bin/curl -s "http://admin.britanniavod.carnivaluk.com/location/private/refreshcurrent" >>/root/weatherupdate/refreshcurrent.log 2>&1
-
-#echo >>/root/weatherupdate/refreshforecast.log
-#date >>/root/weatherupdate/refreshforecast.log
-#/usr/bin/curl -s "http://admin.britanniavod.carnivaluk.com/location/private/refreshforecast" >>/root/weatherupdate/refreshforecast.log 2>&1
