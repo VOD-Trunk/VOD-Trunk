099c1cd847fe310ce48fd322cda66575aa976986
root <root@hsc.com>
2020-07-14 19:51:08 +0530

    updated on Tue Jul 14 19:51:08 IST 2020

---
+++ checkArtifactProperty.sh
@@ -0,0 +1,34 @@
+#!bin/bash
+
+Deployment_env=$1
+Activity=$2
+Release_version=$3
+UserName=$4
+Password=$5
+
+
+if [ "$Deployment_env" == "Support" ] && [ "$Activity" == "Deploy" ]
+then
+    #isQADone=`curl -sS -u "$UserName":"$Password" -X GET 'http://artifactory.tools.ocean.com/artifactory/api/storage/libs-release-local/com/uievolution/exm/exm/"$Release_version"?properties=QA' | grep "Done" | wc -l`
+    isQADone=1
+
+    echo "$isQADone"
+
+    if [ ${isQADone} -eq 1 ]
+    then
+        echo "We are good to deploy in Support environment."
+    else
+         exit 1
+    fi
+
+else
+
+    #isSupportDone=`curl -sS -u "${UserName}":"${Password}" -X GET 'http://artifactory.tools.ocean.com/artifactory/api/storage/libs-release-local/com/uievolution/exm/exm/"${Release_version}"?properties=Support' | grep "Done" | wc -l`
+    isSupportDone=1
+    if [ $isSupportDone -eq 1 ]
+    then
+        echo "We are good to deploy in Production environment."
+    else
+         exit 1
+    fi
+fi
