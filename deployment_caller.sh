5a52d5e75c9ceee574b8f8ffcc352ab7c6dcfdf2
root <root@hsc.com>
2020-07-20 14:05:31 +0530

    updated on Mon Jul 20 14:05:31 IST 2020

--- deployment_caller.sh
+++ deployment_caller.sh
@@ -18,10 +18,8 @@ then
 	echo
 	sshpass -p "Carnival@123" ssh root@$env 'if [ ! -d /root/Releases ]; then mkdir -p /root/Releases; else for folder in `ls /root/Releases`; do rm -rf /root/Releases/$folder; done; fi'
 	sshpass -p "Carnival@123" scp -r $workspace/Releases/$release $workspace/tmp root@$env:/root/Releases
-	for server in app01 app02;
-	do
-		sshpass -p "Carnival@123" ssh root@$env 'ssh $server' "bash -s" -- < $workspace/deploy_on_server.sh -d "$release" "$component" "$abort_on_fail" > $workspace/logs/"${logfile}"
-	done
+	sshpass -p "Carnival@123" ssh root@$env "bash -s" -- < $workspace/deploy_on_server.sh -d "$release" "$component" "$abort_on_fail" > $workspace/logs/"${logfile}"
+	sshpass -p "Carnival@123" ssh root@$env 'ssh app02' "bash -s" -- < $workspace/deploy_on_server.sh -d "$release" "$component" "$abort_on_fail" >> $workspace/logs/"${logfile}"
 	cat $workspace/logs/${logfile}
 elif [ "$action" == "Rollback" ]
 then
