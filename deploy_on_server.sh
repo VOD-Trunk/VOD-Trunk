8c808bc19be040ea17f2f675f98c0c0e27a65a33
VOD-Trunk <abhishek.chadha@hsc.com>
2020-08-30 15:27:36 +0530

    updated with local values

--- deploy_on_server.sh
+++ deploy_on_server.sh
@@ -1144,10 +1144,10 @@ do
 done
 
 
-if [ "$server" == "app01" ] && [ "$transfer_flag" == "true" ] || [ "$action" == "-d" ]
-then
-	log
-	log "Transferring artifacts to app02."
-	log
-	ssh app02 'if [ ! -d /root/Releases ]; then mkdir -p /root/Releases; else for folder in `ls /root/Releases`; do if [ `echo ${folder} | grep "_" | wc -l` -eq 0 ]; then mv /root/Releases/${folder} /root/Releases/${folder}_`date +%Y_%m_%d__%H_%M_%S`; fi; done; fi' && scp -r /root/Releases/$new_release /root/Releases/tmp  app02:/root/Releases
-fi
+#if [ "$server" == "app01" ] && [ "$transfer_flag" == "true" ] || [ "$action" == "-d" ]
+#then
+#	log
+#	log "Transferring artifacts to app02."
+#	log
+#	ssh app02 'if [ ! -d /root/Releases ]; then mkdir -p /root/Releases; else for folder in `ls /root/Releases`; do if [ `echo ${folder} | grep "_" | wc -l` -eq 0 ]; then mv /root/Releases/${folder} /root/Releases/${folder}_`date +%Y_%m_%d__%H_%M_%S`; fi; done; fi' && scp -r /root/Releases/$new_release /root/Releases/tmp  app02:/root/Releases
+#fi
