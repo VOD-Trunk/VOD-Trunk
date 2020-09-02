dbd1778b6cd682525e9b8426f321619101c51ac8
VOD-Trunk <abhishek.chadha@hsc.com>
2020-09-02 10:57:55 +0530

    updated on Wed, Sep  2, 2020 10:57:55 AM

--- deploy_on_server.sh
+++ deploy_on_server.sh
@@ -545,12 +545,12 @@ deploy_new_build() {
 		log
 		log "Copying properties.uie file to $releases_path/releases/$new_release"
 		log
-		#if [ "$component" == "nacos" ]
-		#then
-			cp $releases_path/properties.uie $releases_path/releases/$new_release
-		#else
-		#	cp $releases_path/Backup/$new_release/*.properties $releases_path/releases/$new_release
-		#fi
+		if [ "$component" == "nacos" ]
+		then
+			cp $releases_path/Backup/$new_release/properties.uie $releases_path/releases/$new_release
+		else
+			cp $releases_path/Backup/$new_release/*.properties $releases_path/releases/$new_release
+		fi
 
 		log "Current properties.uie symlink is :"
 		log
@@ -562,12 +562,12 @@ deploy_new_build() {
 		log "Creating new symlink properties.uie ..."
 		log
 
-		#if [ "$component" == "nacos" ]
-		#then
+		if [ "$component" == "nacos" ]
+		then
 			ln -s $releases_path/releases/$new_release/properties.uie $releases_path/properties.uie
-		#else
-		#	ln -s $releases_path/releases/$new_release/*.properties $releases_path/properties.uie
-		#fi
+		else
+			ln -s $releases_path/releases/$new_release/*.properties $releases_path/properties.uie
+		fi
 
 		log "New properties.uie symlink is :"
 		log
