4ada5f8a262b9557008a44810c3a7ebb84f7570a
VOD-Trunk <abhishek.chadha@hsc.com>
2020-09-07 14:22:52 +0530

    updated on Mon, Sep  7, 2020  2:22:52 PM

--- deploy_on_server.sh
+++ deploy_on_server.sh
@@ -365,7 +365,7 @@ deploy_new_build() {
 		unzip -qq /root/Releases/$new_release/db-upgrade-dir/xicms-2.64.0-db-upgrade.zip -d /root/Releases/$new_release/db-upgrade-dir
 		chmod +x /root/Releases/$new_release/db-upgrade-dir/xicms-2.64.0-db-upgrade/db-script.sh
 		cd /root/Releases/$new_release/db-upgrade-dir/xicms-2.64.0-db-upgrade
-		printf 'uie123\n' | ./db-script.sh --upgrade
+		printf 'uie123\n' | ./db-script.sh --upgrade &>/dev/null
 		
 		count=`cat /root/update/2.64.0/db-upgrade-dir/xicms-2.64.0-db-upgrade/2.64.0-dboper-*.log | grep -w "Liquibase Update Successful" | wc -l`
 		 
