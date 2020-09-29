64ab86bf61688ab4cb6d89a5b29e3756515ba9d5
VOD-Trunk <abhishek.chadha@hsc.com>
2020-09-29 16:44:45 +0530

    change for reading IP

--- fetchBinary.py
+++ fetchBinary.py
@@ -258,11 +258,11 @@ with open(logfile_path, 'w+') as logfile:
                                 r = json.load(f)
                                 ipaddr_json = ast.literal_eval(json.dumps(r))
                                 if shipName == "XS" or shipName == "HSC_Test":
-                                    ipaddr = ipaddr_json["jenkins"]["environments"]["QA"][shipName]
+                                    ipaddr = ipaddr_json["jenkins"]["environments"]["QA"][0][shipName]
                                 elif shipName == "SUPPORT":
-                                    ipaddr = ipaddr_json["jenkins"]["environments"]["SUPPORT"][shipName]
+                                    ipaddr = ipaddr_json["jenkins"]["environments"]["SUPPORT"][0][shipName]
                                 else:
-                                    ipaddr = ipaddr_json["jenkins"]["environments"]["PRODUCTION"][shipName]
+                                    ipaddr = ipaddr_json["jenkins"]["environments"]["PRODUCTION"][0][shipName]
                                 log("\nShip " + shipName + " is ready for release deployment. Initiating transfer of artifacts to " + ipaddr)
 
                             with open(scheduled_ships_path, 'a+') as f:
