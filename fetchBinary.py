b176ad9ccd228babd21bff35e32b39e526b040f3
VOD-Trunk <abhishek.chadha@hsc.com>
2020-10-07 00:29:01 +0530

    Added confluence header check

--- fetchBinary.py
+++ fetchBinary.py
@@ -109,7 +109,7 @@ with open(logfile_path, 'w+') as logfile:
                 firstRowColumnNames = re.split(r'\s{2,}', columnValue)
                 log(str(firstRowColumnNames))
                 if len(firstRowColumnNames) != 7:    #count of columns headers should be 7 fixed.
-                    log("\n\nERROR : The table structure on confluence page is not correct. There should only be seven column headers and in this order : Component, Revision Number, Build #, TAG, Artifact, md5sum, Modified over baseline \n\n")
+                    log("\n\nERROR : The table structure on confluence page is not correct. There should be exactly seven column headers and in this order : Component, Revision Number, Build #, TAG, Artifact, md5sum, Modified over baseline \n\n")
                     exit(1)
 
                 tableHeaders=["Component","Revision Number","Build #","TAG","Artifact","md5sum","Modified over baseline"]
