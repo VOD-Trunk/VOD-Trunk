e740a9ad4b4732b70e63854ba42d9a62dc81a5ae
VOD-Trunk <abhishek.chadha@hsc.com>
2020-11-08 18:37:29 +0530

    changes for support setup

--- deploy_on_server.sh
+++ deploy_on_server.sh
@@ -1335,6 +1335,7 @@ case "${1}" in
 		      	servers="app02 media01 media02 lb01 lb02"
 				for targetServer in servers
 				do
+					log "Abhishek : Copying to $targetServer"
 					ssh $targetServer 'if [ ! -d /root/Releases ]; then mkdir -p /root/Releases; else for folder in `ls /root/Releases`; do if [ `echo ${folder} | grep "_" | wc -l` -eq 0 ]; then mv /root/Releases/${folder} /root/Releases/${folder}_`date +%Y_%m_%d__%H_%M_%S`; fi; done; fi'
 					ssh $targetServer 'if [ ! -d /root/Releases/Config_Files ]; then mkdir -p /root/Releases/Config_Files; else rm -rf /root/Releases/Config_Files/*'
 					# ssh media01 'if [ ! -d /root/Releases ]; then mkdir -p /root/Releases; else for folder in `ls /root/Releases`; do if [ `echo ${folder} | grep "_" | wc -l` -eq 0 ]; then mv /root/Releases/${folder} /root/Releases/${folder}_`date +%Y_%m_%d__%H_%M_%S`; fi; done; fi'
