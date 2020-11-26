0da147f4bbb1899c89ca8e6537b248066704bf3e
VOD-Trunk <63137590+VOD-Trunk@users.noreply.github.com>
2020-11-26 23:35:24 +0530

    Update fetch_files.sh

--- fetch_files.sh
+++ fetch_files.sh
@@ -1,5 +1,5 @@
-temp='/home/config_files/temp.txt'
-master='/home/config_files/Config_Files_master.txt'
+temp='$WORKSPACE/temp.txt'
+master='$WORKSPACE/Config_Files_master.txt'
 rm -f /home/config_files/app01/*
 rm -f /home/config_files/app02/*
 rm -f /home/config_files/media01/*
