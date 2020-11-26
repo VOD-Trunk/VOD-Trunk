48584b59205f1f48f3dff632c1c1abb69b250a20
VOD-Trunk <63137590+VOD-Trunk@users.noreply.github.com>
2020-11-26 23:48:39 +0530

    Update update_master_file.sh

--- update_master_file.sh
+++ update_master_file.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-ip=`grep "ip=" /home/vod/properties/path.txt | cut -d\' -f2`
-USERNAME=`grep USERNAME /home/vod/properties/ship_credentials.txt | cut -d\' -f2`
-PASS=`grep PASS /home/vod/properties/ship_credentials.txt | cut -d\' -f2`
-for line in `cat $ip`
-do
-	HOSTNAME=`echo $line | cut -d ',' -f 2`
-	ship=`echo $line | cut -d ',' -f 1`
+$WORKSPACE=$1
+#ip=`grep "ip=" /home/vod/properties/path.txt | cut -d\' -f2`
+#USERNAME=`grep USERNAME /home/vod/properties/ship_credentials.txt | cut -d\' -f2`
+#PASS=`grep PASS /home/vod/properties/ship_credentials.txt | cut -d\' -f2`
+#for line in `cat $ip`
+#do
+#HOSTNAME=`echo $line | cut -d ',' -f 2`
+USERNAME='root'
+PASS='not4dev!'
+HOSTNAME='192.168.248.161'
+ship=SUPPORT
 	
-	echo "Copying master file to $ship"
-	#sshpass -p ${PASS} scp /home/vod/properties/Config_Files_master.txt ${USERNAME}@${HOSTNAME}:/home/config_files/
-	sshpass -p ${PASS} scp /home/vod/scripts/fetch_files.sh ${USERNAME}@${HOSTNAME}:/home/config_files/
-done
+echo "Copying master file to $ship"
+sshpass -p ${PASS} scp $WORKSPACE/Config_Files_master.txt ${USERNAME}@${HOSTNAME}:/home/config_files/
+sshpass -p ${PASS} scp $WORKSPACE/fetch_files.sh ${USERNAME}@${HOSTNAME}:/home/config_files/
+#done
