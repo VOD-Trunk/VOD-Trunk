6892d04a83c2d13ed3eb28e63e00d32d43a30139
VOD-Trunk <63137590+VOD-Trunk@users.noreply.github.com>
2020-11-30 17:08:21 +0530

    Update find_config_diff.sh

--- find_config_diff.sh
+++ find_config_diff.sh
@@ -30,5 +30,5 @@ echo "Copying files from app01 of $Ship_NAME to local GIT path."
 
 
 #Copy the consolidated config files from server to local git path.
-sshpass -p ${PASS} scp ${USERNAME}@${HOSTNAME}:/home/config_files/config_files.tar.gz ${git_path}/Ship_Current_Files/${ship}
-cd ${git_path}/Ship_Current_Files/${ship} && tar -xzf config_files.tar.gz && rm -f config_files.tar.gz
+sshpass -p ${PASS} scp ${USERNAME}@${HOSTNAME}:/home/config_files/config_files.tar.gz ${git_path}/Ship_Current_Files/${Ship_NAME}
+cd ${git_path}/Ship_Current_Files/${Ship_NAME} && tar -xzf config_files.tar.gz && rm -f config_files.tar.gz
