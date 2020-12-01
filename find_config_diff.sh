5fd2a9ff38792dd3d84971a56b7505a595aa7f5c
VOD-Trunk <63137590+VOD-Trunk@users.noreply.github.com>
2020-12-01 17:11:17 +0530

    Update find_config_diff.sh

--- find_config_diff.sh
+++ find_config_diff.sh
@@ -8,12 +8,15 @@ HOSTNAME='192.168.248.161'
 Ship_NAME=SUPPORT
 log_file='config.log'
 
-if [ ! -d $WORKSPACE/Config_Files/VOD-Trunk/Ship_Current_Files/$Ship_NAME ]
+if [ ! -d $WORKSPACE/Ship_Config/VOD-Trunk/Ship_Current_Files/$Ship_NAME ]
 then
-mkdir -p $WORKSPACE/Config_Files/VOD-Trunk/Ship_Current_Files/$Ship_NAME
+mkdir -p $WORKSPACE/Ship_Config/VOD-Trunk/Ship_Current_Files/$Ship_NAME
 fi
 
-git_path=$WORKSPACE/Config_Files/VOD-Trunk
+mkdir -p $WORKSPACE/Config_Files
+
+git_path=$WORKSPACE/Ship_Config/VOD-Trunk
+push_path=$WORKSPACE/Config_Files
 
 #Run the script fetch_files.sh on respective servers and move to next hostname if ssh takes more than 20 seconds.
 sshpass -p ${PASS} ssh -o "StrictHostKeyChecking=no" ${USERNAME}@${HOSTNAME} 'cd /home/config_files/ && rm -f config_files.tar.gz && ./fetch_files.sh && tar -czf config_files.tar.gz app01 app02 media01 media02 lb01 lb02' 2>/dev/null
@@ -30,19 +33,20 @@ echo "Copying files from app01 of $Ship_NAME to local GIT path."
 
 
 #Copy the consolidated config files from server to local git path.
-sshpass -p ${PASS} scp ${USERNAME}@${HOSTNAME}:/home/config_files/config_files.tar.gz ${git_path}/Ship_Current_Files/${Ship_NAME}
+sshpass -p ${PASS} scp ${USERNAME}@${HOSTNAME}:/home/config_files/config_files.tar.gz ${git_path}/Ship_Current_File/${Ship_NAME}
 cd ${git_path}/Ship_Current_Files/${Ship_NAME} && tar -xzf config_files.tar.gz && rm -f config_files.tar.gz
 
 
 #Push All config files to git
-cd ${git_path}
+cd ${push_path}
 git init
-git remote add https://github.com/VOD-Trunk/VOD-Trunk.git
+git remote add VOD-Trunk https://github.com/VOD-Trunk/VOD-Trunk.git
 git checkout develop
 git pull origin develop
+cp -r ${git_path}/Ship_Current_File/${Ship_NAME} $WORKSPACE/Config_Files/VOD-Trunk
 git add .
 git commit -m "Updating $Ship_NAME GIT files with updated values on $Ship_NAME config"
-git push origin develop
+git push --all
 
 echo "======================================================================================================================="
 echo  "GIT Changes Pushed"
